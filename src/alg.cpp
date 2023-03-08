63bb19a45666179048ea8273f0b53eaea3cd7faf
Darya Volkova <125033233+DaryaVolkova@users.noreply.github.com>
2023-03-08 17:35:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,31 +1,31 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int left = 0;
-	int right = size;
-	int mid;
-	int tmp_left, tmp_right;
-	int res;
-	while (left < right) {
-		mid = (left + right) / 2;
-		if (value > arr[mid]) {
-			left = mid + 1;
-		} else {
-			right = mid;
-		}
-	}
-	tmp_left = left;
-	left = 0;
-	right = size;
-	while (left < right) {
-		mid = (left + right) / 2;
-		if (value < arr[mid]) {
-			right = mid;
-		} else {
-			left = mid + 1;
-		}
-	}
-	tmp_right = right;
-	res = tmp_right - tmp_left;
-	return res;
+    int left = 0;
+    int right = size;
+    int mid;
+    int tmp_left, tmp_right;
+    int res;
+    while (left < right) {
+        mid = (left + right) / 2;
+        if (value > arr[mid]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    tmp_left = left;
+    left = 0;
+    right = size;
+    while (left < right) {
+        mid = (left + right) / 2;
+        if (value < arr[mid]) {
+            right = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+    tmp_right = right;
+    res = tmp_right - tmp_left;
+    return res;
 }
